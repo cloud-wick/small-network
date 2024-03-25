d12d4d79fc4fbc5855f791c9314811690268782c
sayurud <sayurud@mitesp.com>
2024-03-25 15:05:47 +0530

    add alb sec grp

---
+++ modules/security/main.tf
@@ -0,0 +1,33 @@
+# alb sec grp
+resource "aws_security_group" "alb-sec_grp" {
+  name = var.alb-name
+  description = "enable https/http on port 443/80"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "http access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   ingress {
+    description = "https access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.alb-name
+  }
+}
